8519e03f7e05bedee37f6a9d9ee5cdb20cc2bca4
Hackbright Student <info@hackbrightacademy.com>
2014-02-07 14:39:55 -0800

    Added validation for file existence.

--- lettercount.py
+++ lettercount.py
@@ -10,47 +10,49 @@ from os.path import exists
 
 # Takes in Python script and one file as arguments. 
 script, input_file = argv
+Valid = True
 
 # Check if input_file exists.
-# if not exists(input_file):
-#     print "%r does not exist!" % input_file
-#     break
+if not exists(input_file):
+    print "%r does not exist!" % input_file
+    Valid = False
 
+while Valid:
 # Opens and reads input_file.
-in_file = open(input_file)
-indata = in_file.read()
+    in_file = open(input_file)
+    indata = in_file.read()
 
-# count = 0
-# for char in indata:
-#     print char
-#     count += 1
+    # count = 0
+    # for char in indata:
+    #     print char
+    #     count += 1
 
-# print count
+    # print count
 
-# Convert all letters to lowercase.
-indata = indata.lower()
-# Create list of alphabet, lowercase. 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# Create list to keep track of counts of each letter. 
-# Need to replace all elements with proper numbers later.
-letter_count = range(27)
+    # Convert all letters to lowercase.
+    indata = indata.lower()
+    # Create list of alphabet, lowercase. 
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    # Create list to keep track of counts of each letter. 
+    # Need to replace all elements with proper numbers later.
+    letter_count = range(27)
 
-# Set all elements in letter_count to number 0.
-for i in range(len(letter_count)):
-    # index needs to reach 27, i = 27 for the 26th element of the alphabet list
-    # print i
-    # print "length", len(letter_count)
-    letter_count[i] = 0
+    # Set all elements in letter_count to number 0.
+    for i in range(len(letter_count)):
+        # index needs to reach 27, i = 27 for the 26th element of the alphabet list
+        # print i
+        # print "length", len(letter_count)
+        letter_count[i] = 0
 
-# Counts how many times each letter occurs in indata.
-for i in range(len(indata)): 
-    
-    for letter in range(len(alphabet)): 
-        if indata[i] == alphabet[letter]:
-            print indata[i]
-            letter_count[letter] += 1
+    # Counts how many times each letter occurs in indata.
+    for i in range(len(indata)): 
+        for letter in range(len(alphabet)): 
+            if indata[i] == alphabet[letter]:
+                letter_count[letter] += 1
 
-print letter_count
+    for i in range(len(letter_count)):
+        print letter_count[i]
 
+    Valid = False
             
 
