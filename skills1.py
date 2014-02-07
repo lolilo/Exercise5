0b07483551c8c7b6685c3dc9aeb09742c7118f39
Hackbright Student <info@hackbrightacademy.com>
2014-02-07 15:54:08 -0800

    Remove even numbers from some_list.

--- skills1.py
+++ skills1.py
@@ -5,47 +5,49 @@ def all_odd(some_list):
   # Check for valid input.
 
   numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
   # If you're deleting stuff from a list, be careful! 
   # Iterate backwards so that you don't mess up the index.
   # Don't forget the craziness with memory. 
   # list.reverse() doesn't return anything!
 
-  reversed_list_index = range(len(some_list))
-
-  reversed_list_index.reverse()
-
-  print reversed_list_index
+  # reversed_list_index = range(len(some_list))
+  # reversed_list_index.reverse()
+  # The one below is shorter/nicer -- we recreated this in Exercise 4! :D
+  reversed_list_index = range(len(some_list))[::-1]
 
   for element_index in reversed_list_index:
     print "\n"
     # If element is not of type int, remove element. 
-
-    # print "element index: ", element_index
-    # print "element: ", some_list[element_index]
-
+        # print "element index: ", element_index
+        # print "element: ", some_list[element_index]
 
     if type(some_list[element_index]) == int:
       print some_list[element_index], type(element_index)
+      if some_list[element_index] % 2 == 0:
+        del some_list[element_index]
       
     else:  
       # For each char in the element
       for char in some_list[element_index]:
-
         # If element is not a number, remove element.
         if char not in numbers:
-          # print "deleting %r" % some_list[element_index]
+              # print "deleting %r" % some_list[element_index]
           del some_list[element_index]
           break 
 
+  # If element is a number, remove if even number.
+  # Must create a new reversed_list_index, since we altered some_list. 
+  # reversed_list_index = range(len(some_list))[::-1]
+
+  # for element_index in 
+
   print some_list
 
-  # If element is a number, remove if even number.
 
-    #   if some_list[i] % 2 
-    # return []
 
-all_odd([17, 21, "ha", 7, "yo", "two4you", 9])
+
+
+all_odd([17, 21, "ha", 7, "yo", 2, 48, "two4you", 9])
 
 # Write a function that takes a list and returns a new list with only the even numbers.
 def all_even(some_list):
