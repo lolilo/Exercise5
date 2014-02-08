6d51a8c9fc71870fdc00d98d6be4664d8e051832
Hackbright Student <info@hackbrightacademy.com>
2014-02-07 17:19:34 -0800

    Revised with ord().

--- lettercount.py
+++ lettercount.py
@@ -18,7 +18,7 @@ if not exists(input_file):
     Valid = False
 
 
-## Use if, not while loop
+## Use if, not while-loop
 if Valid:
 # Opens and reads input_file.
     in_file = open(input_file)
@@ -27,7 +27,7 @@ if Valid:
     # Convert all letters to lowercase.
     indata = indata.lower()
     # Create list of alphabet, lowercase. 
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     # Create list to keep track of counts of each letter. 
 
     letter_count = [0] * 27
@@ -35,11 +35,26 @@ if Valid:
 
     # Counts how many times each letter occurs in indata.
 
-    # With ord(), don't need the second for loop to loop through the alphabet. 
-    for i in range(len(indata)): 
-        for letter in range(len(alphabet)): 
-            if indata[i] == alphabet[letter]:
-                letter_count[letter] += 1
+    # for i in range(len(indata)): 
+    #     for letter in range(len(alphabet)): 
+    #         if indata[i] == alphabet[letter]:
+    #             letter_count[letter] += 1
+
+    # With ord(), don't need the second for-loop to iterate through the alphabet. 
+
+    # Think of this as looking at numbers, from 97 to 122, inclusive. 
+    # When you see 97, increment the counter for 97. 
+    # Access the proper list index an increase the counter. 
+    # For 97, it's letter_count[0] += 1,
+    # For 98, it's letter_count[1] += 1, etc.
+
+    for i in range(len(indata)):
+        # Check for non-alphabetic characters; skip over those. 
+        if ord(indata[i]) < 97 or ord(indata[i]) > 122:
+            pass
+        else: 
+            # Increment the proper letter_counter element.
+            letter_count[ord(indata[i]) - 97] += 1 
 
     for i in range(len(letter_count)):
         print letter_count[i]
