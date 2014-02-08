f9b588197b878ba1d30207ccfa03cf66203a9fc4
Hackbright Student <info@hackbrightacademy.com>
2014-02-07 16:56:48 -0800

    Code review by Liz.

--- lettercount.py
+++ lettercount.py
@@ -17,7 +17,9 @@ if not exists(input_file):
     print "%r does not exist!" % input_file
     Valid = False
 
-while Valid:
+
+## Use if, not while loop
+if Valid:
 # Opens and reads input_file.
     in_file = open(input_file)
     indata = in_file.read()
@@ -27,17 +29,13 @@ while Valid:
     # Create list of alphabet, lowercase. 
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     # Create list to keep track of counts of each letter. 
-    # Need to replace all elements with proper numbers later.
-    letter_count = range(27)
 
-    # Set all elements in letter_count to number 0.
-    for i in range(len(letter_count)):
-        # index needs to reach 27, i = 27 for the 26th element of the alphabet list
-        # print i
-        # print "length", len(letter_count)
-        letter_count[i] = 0
+    letter_count = [0] * 27
+    # ^ Liz's better way. 
 
     # Counts how many times each letter occurs in indata.
+
+    # With ord(), don't need the second for loop to loop through the alphabet. 
     for i in range(len(indata)): 
         for letter in range(len(alphabet)): 
             if indata[i] == alphabet[letter]:
