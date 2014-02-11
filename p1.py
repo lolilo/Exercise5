28d6e75f7ab6f9d6b6b8df4a88095dae3f8a8cc8
Hackbright Student <info@hackbrightacademy.com>
2014-02-10 19:15:35 -0800

    Completed, Nick Av code reviewed and passed.

--- p1.py
+++ p1.py
@@ -11,14 +11,13 @@
 # Recursively move a file or directory (src) to another location (dsf). 
 # Destination directory must not already exist. 
 
-import os
+import os, shutil
 
 # 1. Create 26 directories in the current directory, one for each letter of the alphabet:
 #     ./a/
 #     ./b/
 #     ./c/
 #     etc.
-
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -35,3 +34,36 @@ for letter in alphabet:
 #     'bartholomew.txt' would go into 'b'.
 
 # Need to open original_files to look at files within.
+
+from sys import argv
+
+# The command 'exists' returns True if a file exists, based on its name in a string as an argument. 
+# It returns False if not.
+from os.path import exists
+
+# Takes in Python script and one file as arguments. 
+script, input_file = argv #input_file will be a directory name
+isValid = True
+
+# Check if input_file exists.
+if not exists(input_file):
+    print "%r does not exist!" % input_file
+    isValid = False
+
+if isValid:
+    # print os.listdir(input_file) #input_file is a string, the user-given directory
+
+    # Iterate through a list containing the names of the entries in the input_file directory.
+    for f in os.listdir(input_file):
+        # ordinal_number_of_first_letter = ord(f[0]) - 97
+        # print str(ord(f[0]) - 97)
+        print "Moving %s to directory %s." %(f, f[0])
+        # need to change directory to access f
+        # os.chdir(input_file)
+        # But! We still need to access the letter directory which we were originally working...
+        # print input_file + "/" + f
+        shutil.move("./" + input_file + "/" + f, f[0])
+        print "Success!"
+
+
+
