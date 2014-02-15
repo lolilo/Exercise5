330073b64e3840620c3533a38029f63cb77aff47
Hackbright Student <info@hackbrightacademy.com>
2014-02-14 18:00:04 -0800

    ord

--- p1.py
+++ p1.py
@@ -20,7 +20,8 @@ import os, shutil
 #     etc.
 
 # Could also do this with ord(). 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = range(ord('a'), ord('{'))
 
 for letter in alphabet: 
     directory = letter
